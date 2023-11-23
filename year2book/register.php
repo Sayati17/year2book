92028a9df51203897e30242bbefbed9c9de32d1c
William Sulasman <104675468+Sayati17@users.noreply.github.com>
2023-11-23 23:13:53 +0700

    Menghapus Code

--- year2book/register.php
+++ year2book/register.php
@@ -1,11 +1,5 @@
 <?php
   session_start();
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate CSRF token
-    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
-        die("Invalid CSRF token");
-    }
-  }
 ?>
 
 <!DOCTYPE html>
