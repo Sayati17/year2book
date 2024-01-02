acf04aef8d0a5930892cbcd0bf611b0adb462e7a
Ryan <77276316+Ryan3954@users.noreply.github.com>
2024-01-02 23:18:45 +0700

    csrf controler login

--- year2book/controller/login_page_control.php
+++ year2book/controller/login_page_control.php
@@ -3,7 +3,9 @@
     require "../connection/conn.php";
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-
+        if(!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']){
+            die("Invalid CSRF Token");
+        }
         $username = $_POST['username'];
         $password = $_POST['password'];
 
